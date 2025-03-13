0601e9c2217ce04ebbf6e123437cd291d6ec7ab0
Rob Gibbon <robgibbon@mac.lan>
2025-03-13 09:49:37 +0100

    initial commit

---
+++ tool.py
@@ -0,0 +1,51 @@
+from schema import Literal, Schema
+import os
+from griptape.artifacts import BaseArtifact, ErrorArtifact, TextArtifact
+from griptape.tools import BaseTool
+from griptape.utils.decorators import activity
+from zdesk import Zendesk
+
+class ZendeskTool(BaseTool):
+
+    zdesk_url = os.environ['ZENDESK_URL']
+    zdesk_email = os.environ['ZENDESK_EMAIL']
+    zdesk_password = os.environ['ZENDESK_PASSWORD']
+    zdesk_oauth = os.environ['ZENDESK_OAUTH']
+    zdesk_token = os.environ['ZENDESK_TOKEN'] 
+    zendesk = Zendesk(zdesk_url=zdesk_url, zdesk_password=zdesk_password, zdesk_email=zdesk_email, zdesk_oauth=zdesk_oauth, zdesk_token=zdesk_token)
+         
+    @activity(
+        config={
+            "description": "Create Zendesk customer support tickets",
+            "schema": Schema(
+                {
+                    Literal("requeester_name"): str,
+                    Literal("requester_email"): str,
+                    Literal("subject"): str,
+                    Literal("description"): str,
+                },
+            ),
+        },
+    )
+    def new_ticket(self, params: dict) -> BaseArtifact: 
+        try:
+            requester_name = params["values"]["requester_name"]
+            requester_email = params["values"]["requester_email"]
+            subject = params["values"]["subject"]
+            description = params["values"]["description"]
+            new_ticket = {
+                'ticket': {
+                    'requester_name': requester_name,
+                    'requester_email': requester_email,
+                    'subject': subject,
+                    'description': description,
+                }
+            }
+
+            result = zendesk.ticket_create(data=new_ticket)
+        except Exception as e:
+            return ErrorArtifact(f"error creating ticket: {e})
+
+def init_tool() -> ZendeskTool:
+
+    return ZendeskTool()
